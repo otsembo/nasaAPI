c68879218e36f1bf7c19b709f06c829060c1bfb0
otsembo <okumu.otsembo@gmail.com>
2022-03-04 16:21:42 +0300

    Model: Added APOD interface

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -26,10 +26,4 @@ describe('AppComponent', () => {
     expect(app.title).toEqual('NasaProject');
   });
 
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('NasaProject app is running!');
-  });
 });
