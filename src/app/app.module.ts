3bd424663101f57551f27b512611add1179e2ba6
otsembo <okumu.otsembo@gmail.com>
2022-03-08 10:32:13 +0300

    UI: Generated mars rover component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './presentation/home/home.component';
+import { MarsRoverComponent } from './presentation/mars-rover/mars-rover.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    MarsRoverComponent
   ],
   imports: [
     BrowserModule,
